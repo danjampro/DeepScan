9bb86c6bd1a3d401906a85221eed4e43d2592fac
Daniel Prole <danjampro@Daniels-MacBook-Pro.local>
2017-11-09 12:58:36 +0000

    gzip data

--- vignettes/ngvs_small.py
+++ vignettes/ngvs_small.py
@@ -11,11 +11,21 @@ import urllib.request
 import numpy as np
 from deepscan import utils
 import matplotlib.pyplot as plt
+import gzip
+import shutil
 
-url = 'https://raw.github.com/danjampro/DeepScan/master/data/ngvs_small.fits'
+#==============================================================================
+#Online data retrieval
+url = 'https://raw.github.com/danjampro/DeepScan/master/data/ngvs_small.fits.gz'
+fname_gz = os.path.join(os.getcwd(), 'ngvs_small.fits.gz')
 fname = os.path.join(os.getcwd(), 'ngvs_small.fits')
 urllib.request.urlretrieve(url, fname)
+with gzip.open(fname_gz, 'rb') as f_in, open(fname, 'wb') as f_out:
+    shutil.copyfileobj(f_in, f_out)
+
 data = utils.read_fits(fname)
+#==============================================================================
+
 
 
 plt.figure(); plt.imshow(np.arcsinh(data), cmap='binary')
